d92806b19004ea14d086694aec94417b56685033
Ricardo2930 <rhuaytan@gmail.com>
2022-10-19 23:53:23 -0500

    api balon de oro

---
+++ bootstrap/index.js
@@ -0,0 +1,20 @@
+const listPlayers = document.querySelector("#list-players");
+const urlApi = "http://localhost:4000/jugadores-balon-oro";
+
+async function renderPlayers() {
+  try {
+    const response = await fetch (urlApi);  
+    const data = await response.json();
+    console.log (data);
+
+    data.forEach((player) => {
+      listPlayers.innerHTML += `<my-card title=${player.name} img=${player.img} message="El ganador del año ${player.year}"></my-card>`;
+    });
+  } 
+  catch (error) {
+    // si algo sale mal en el try acá vamos a poder manejar este error
+    console.log(error);
+  }
+}
+
+renderPlayers();
--- javascript-semana5/semana7/todoapi/index.js
+++ javascript-semana5/semana7/todoapi/index.js
@@ -30,4 +30,36 @@ const tasks = [];
         });
  });
 
+ app.get("/jugadores-balon-oro", (req, res) => {
+    // winners of gold ball
+    const players = [
+      {
+        name: "Karin Benzema",
+        year: 2022,
+        img: "https://e00-marca.uecdn.es/deporte/balon-de-oro/imagenes/palmares/benzema-2022.jpg",
+      },
+      {
+        name: "Leo Messi",
+        year: 2021,
+        img: "https://e00-marca.uecdn.es/deporte/balon-de-oro/imagenes/palmares/messi-2021.jpg",
+      },
+      {
+        name: "Leo Messi",
+        year: 2019,
+        img: "https://e00-marca.uecdn.es/deporte/balon-de-oro/imagenes/palmares/messi-2019.jpg",
+      },
+      {
+        name: "Luka Modric",
+        year: 2018,
+        img: "https://e00-marca.uecdn.es/deporte/balon-de-oro/imagenes/palmares/modric-2018.jpg",
+      },
+      {
+        name: "Cristiano Ronaldo",
+        year: 2017,
+        img: "https://e00-marca.uecdn.es/deporte/balon-de-oro/imagenes/palmares/cristiano-ronaldo-2017.jpg",
+      },
+    ];
+    res.json(players);
+  });
+
  app.listen(4000);
