25d1bcedd6ca455c108ad7734725201d96c1ef25
Ricardo2930 <rhuaytan@gmail.com>
2022-10-21 21:40:47 -0500

    todoapp init project

---
+++ todo-app/components/card.js
@@ -0,0 +1,38 @@
+class Card extends HTMLElement {
+	constructor() {
+		super();
+		this.class = "card p-3";
+		this.classname = "";
+	}
+
+	static get observedAttributes() {
+		return ["class", "classname"];
+	}
+
+	attributeChangedCallback(prop, oldvalue, newvalue) {
+		this[prop] = newvalue;
+	}
+
+	// Cuando este elemento sea llamado a Renderizarse:
+	connectedCallback() {
+        // Con este if hago referencia al this.class del super, para luego crear una const classList = this.class.split(""), el split convierte convierte el tipo string agregado. agarra al array para separarlo segun lo que separa los elementos del array
+		if (this.class) {
+			const lista_de_clases = this.class.split(" ");
+			lista_de_clases.forEach((xclass) => {
+				this.classList.add(xclass);
+			});
+		}
+
+		if (this.classname) {
+			const lista_de_clases = this.classname.split(" ");
+			lista_de_clases.forEach((xclass) => {
+				this.classList.add(xclass);
+			});
+		}
+	}
+
+	// Cuando este elemento sea quitado de la renderización:
+	disconnectedCallback() {}
+}
+
+customElements.define("my-card", Card);
