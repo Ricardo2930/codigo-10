b0c5a2c8ce2e86a700bae3dde069ef69d09e1bb4
Ricardo2930 <rhuaytan@gmail.com>
2022-11-02 23:15:09 -0500

    update confirm

--- todoreact/todoreact/src/components/TaskCard/index.js
+++ todoreact/todoreact/src/components/TaskCard/index.js
@@ -1,47 +1,62 @@
+import Swal from "sweetalert2";
+
 function TaskCard(props) {
-    const { task, updateTask } = props;
-  
-    const statusClass = {
-      1: "",
-      2: "bg-success",
-      3: "bg-danger",
-    };
-  
-    return (
-      <div
-        className={`mt-3 card p-3 mt-3 shadow-sm bg-opacity-75 ${
-          statusClass[task.status]
-        }`}
-      >
-        <div className="d-flex">
-          {task.status === 1 && (
-            <span className="me-3">
-              <button
-                onClick={() => updateTask(task.id)}
-                className="btn btn-sm btn-outline-primary py-0 small opacity-50 checkButton"
-              >
-                ✓
-              </button>
-            </span>
-          )}
-          <span>{task.name}</span>
-        </div>
-        <hr className="border border-muted border-1" />
-        {task.status === 1 && (
-          <div className="d-flex justify-content-between">
-            <span className="text-muted small">{String(task.createdAt)}</span>
-            <span>
-              <button className="btn btn-sm btn-outline-secondary py-0 small opacity-50">
-                ✎
-              </button>
-              <button className="btn btn-sm btn-outline-danger py-0 small opacity-50 deleteButton">
-                ×
-              </button>
-            </span>
-          </div>
-        )}
-      </div>
-    );
+  const { task, updateTask } = props;
+
+  const statusClass = {
+    1: "bg-primary",
+    2: "bg-success",
+    3: "bg-danger",
+  };
+
+  async function confirmUpdate() {
+    const res = await Swal.fire({
+      title: "Importante",
+      text: "Estas seguro de confirmar que terminaste esta tarea?",
+      showCancelButton: true,
+      showConfirmButton: true,
+    });
+
+    if (res.isConfirmed) {
+      updateTask(task.id);
+    }
   }
-  
-  export default TaskCard;
+
+  return (
+    <div
+      className={`mt-3 card p-3 mt-3 shadow-sm bg-opacity-10 ${
+        statusClass[task.status]
+      }`}
+    >
+      <div className="d-flex">
+        {task.status === 1 && (
+          <span className="me-3">
+            <button
+              onClick={confirmUpdate}
+              className="btn btn-sm btn-outline-primary py-0 small opacity-50 checkButton"
+            >
+              ✓
+            </button>
+          </span>
+        )}
+        <span>{task.name}</span>
+      </div>
+      <hr className="border border-muted border-1" />
+      {task.status === 1 && (
+        <div className="d-flex justify-content-between">
+          <span className="text-muted small">{String(task.createdAt)}</span>
+          <span>
+            <button className="btn btn-sm btn-outline-secondary py-0 small opacity-50">
+              ✎
+            </button>
+            <button className="btn btn-sm btn-outline-danger py-0 small opacity-50 deleteButton">
+              ×
+            </button>
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default TaskCard;
