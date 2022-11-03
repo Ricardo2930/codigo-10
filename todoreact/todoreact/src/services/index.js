79f414102b317af7ee0677a6bafa6efb620884af
Ricardo2930 <rhuaytan@gmail.com>
2022-11-02 21:41:12 -0500

    creando nuestro service

---
+++ todoreact/todoreact/src/services/index.js
@@ -0,0 +1,27 @@
+const url = "https://6363106137f2167d6f716e78.mockapi.io/tareas";
+
+export const get = async () => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const post = async (body) => {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
