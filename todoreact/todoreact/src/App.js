dfdc5eb1fa652eea3618e34372ec1a91e1d48cbe
Ricardo2930 <rhuaytan@gmail.com>
2022-11-02 19:37:20 -0500

    tasks react

--- todoreact/todoreact/src/App.js
+++ todoreact/todoreact/src/App.js
@@ -1,12 +1,34 @@
-import { TaskForm } from "./components";
+import { useState } from "react";
+import { TaskForm, TaskCard } from "./components";
 
 function App() {
+	const [taskList, setTaskList] = useState([]);
+
+	function addTask(text) {
+		const newTask = {
+			text: text,
+			datetime: new Date(),
+		};
+// Paso 01: Extraer la info del tasklist
+// Paso 02: Agregar la nueva tarea
+// Paso 03: Actualizar el estado del tasklist
+// Esto se hace para inidcrale que estmos agregando un nuevo valor al setTaskList, por ende provocamos el RENDER
+
+		setTaskList([...taskList, newTask]);
+		console.log(taskList);
+	}
+
 	return (
 		<div className="container my-5">
 			<h1 className="display-3">Todo App</h1>
-			<TaskForm />
+			<TaskForm onSubmitFunction={addTask} />
 			<div className="text-primary">
 				<hr />
+			</div>
+			<div className="mt-5">
+				{taskList.map((task, index) => (
+					<TaskCard key={index} task={task} />
+				))}
 			</div>
 		</div>
 	);
--- todoreact/todoreact/src/components/index.js
+++ todoreact/todoreact/src/components/index.js
@@ -1,1 +1,2 @@
 export { default as TaskForm } from "./TaskForm/index";
+export { default as TaskCard } from "./TaskCard/index";
