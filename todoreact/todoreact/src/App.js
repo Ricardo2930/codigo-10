74b26ce1fc2eee8c4da84d44945b1e50b1cb6b4a
Ricardo2930 <rhuaytan@gmail.com>
2022-11-02 20:31:31 -0500

    usando mockapi

--- todoreact/todoreact/src/App.js
+++ todoreact/todoreact/src/App.js
@@ -1,37 +1,60 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TaskForm, TaskCard } from "./components";
 
 function App() {
-	const [taskList, setTaskList] = useState([]);
+  const [taskList, setTaskList] = useState([]);
 
-	function addTask(text) {
-		const newTask = {
-			text: text,
-			datetime: new Date(),
-		};
-// Paso 01: Extraer la info del tasklist
-// Paso 02: Agregar la nueva tarea
-// Paso 03: Actualizar el estado del tasklist
-// Esto se hace para inidcrale que estmos agregando un nuevo valor al setTaskList, por ende provocamos el RENDER
+  async function getTasks() {
+    try {
+      const url = "https://6363105c66f75177ea3c9310.mockapi.io/tareas";
+      const response = await fetch(url);
+      const data = await response.json();
+      setTaskList(data);
+    } catch (error) {
+      console.log("Error", error);
+    }
+  }
 
-		setTaskList([...taskList, newTask]);
-		console.log(taskList);
-	}
+  async function addTask(text) {
+    const newTask = JSON.stringify({ name: text, status: 1 });
+    try {
+      const url = "https://6363106137f2167d6f716e78.mockapi.io/tareas";
+      await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: newTask,
+      });
+      await getTasks();
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	return (
-		<div className="container my-5">
-			<h1 className="display-3">Todo App</h1>
-			<TaskForm onSubmitFunction={addTask} />
-			<div className="text-primary">
-				<hr />
-			</div>
-			<div className="mt-5">
-				{taskList.map((task, index) => (
-					<TaskCard key={index} task={task} />
-				))}
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    getTasks();
+    // que provoca el []: Que se ejecute solo una vez
+  }, []);
+
+  return (
+    <div className="container my-5">
+      <h1 className="display-3">Todo App</h1>
+      <TaskForm onSubmitFunction={addTask} />
+      <div className="text-primary">
+        <hr />
+      </div>
+      <div className="mt-5">
+        {taskList.map((task, index) => (
+          <TaskCard key={index} task={task} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
+
+
+
+
