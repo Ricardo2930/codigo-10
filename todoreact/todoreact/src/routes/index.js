2b6689cb6ed09c6ba4f1e35938eaeb9715acffe7
Ricardo2930 <rhuaytan@gmail.com>
2022-11-11 21:25:15 -0500

    Material UI

--- todoreact/todoreact/src/routes/index.js
+++ todoreact/todoreact/src/routes/index.js
@@ -2,7 +2,7 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 // Nuestra vista principal del proyecto esta en APP.JS la copiamos o pasamos a home/index.js
 // Importar nuestras vistas
-import { HomeView, ProfileView, TaskView, LoginView } from "../Pages";
+import { HomeView, ProfileView, TaskView, LoginView, TodoAppView } from "../Pages";
 import { MainLayout } from "../layouts";
 
 // path => el nombre de la ruta / , / login / registro
@@ -13,6 +13,7 @@ const Router = () => {
             <Routes>
                 <Route element={<MainLayout />}>
                     <Route path="/" element={<HomeView />} />
+                    <Route path="/TodoApp" element={<TodoAppView />} />
                     <Route path="/perfil" element={<ProfileView />} />
                     <Route path="/task/:id" element={<TaskView />} />
                 </Route>
