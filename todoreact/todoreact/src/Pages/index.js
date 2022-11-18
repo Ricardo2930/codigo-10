02a652b6001c2905035e15b70468b324bd1fb13e
Ricardo2930 <rhuaytan@gmail.com>
2022-11-17 23:25:26 -0500

    add cartbooks view

--- todoreact/todoreact/src/Pages/index.js
+++ todoreact/todoreact/src/Pages/index.js
@@ -1,3 +1,5 @@
+import { formGroupClasses } from "@mui/material";
+
 // HomeView es un apodo para llamarlo con ese nombre
 export {default as HomeView} from "./Home"
 
@@ -12,3 +14,5 @@ export { default as SignUpView} from "./SignUp";
 export { default as TodoAppView} from "./TodoApp"
 
 export { default as BooksView } from "./Books";
+
+export { default as CarsBooksView} from "./Cars";
--- todoreact/todoreact/src/routes/index.js
+++ todoreact/todoreact/src/routes/index.js
@@ -2,7 +2,7 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 // Nuestra vista principal del proyecto esta en APP.JS la copiamos o pasamos a home/index.js
 // Importar nuestras vistas
-import { HomeView, ProfileView, TaskView, LoginView, SignUpView, TodoAppView, BooksView} from "../Pages";
+import { HomeView, ProfileView, TaskView, LoginView, SignUpView, TodoAppView, BooksView, CarsBooksView} from "../Pages";
 import { MainLayout } from "../layouts";
 
 // path => el nombre de la ruta / , / login / registro
@@ -14,6 +14,7 @@ const Router = () => {
                 <Route element={<MainLayout />}>
                     <Route path="/" element={<HomeView />} />
                     <Route path="/books" element={<BooksView />} />
+                    <Route path="/cars" element={<CarsBooksView />} />
                     <Route path="/TodoApp" element={<TodoAppView />} />
                     <Route path="/perfil" element={<ProfileView />} />
                     <Route path="/task/:id" element={<TaskView />} />
